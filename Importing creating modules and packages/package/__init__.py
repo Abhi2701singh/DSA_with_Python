c995f797ef1ae330dd63e93d3e9db8250491b070
ABHINAV SINGH <abhinavsi434@gmail.com>
2025-04-19 22:55:15 +0530

    commit

---
+++ Importing creating modules and packages/package/__init__.py
@@ -0,0 +1,5 @@
+""" __init__.py  is a special Python file that indicates that the directory it is in should be treated as a package.
+    This file can be empty, but it can also execute the initialization code for the package or set the __all__ variable,
+    which defines the public interface of the package.
+"""
+
---
+++ Importing creating modules and packages/package/maths.py
@@ -0,0 +1,7 @@
+def addition(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtraction(a, b):
+    """Subtract two numbers."""
+    return a - b
